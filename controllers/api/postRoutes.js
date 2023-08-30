5e4db61bd16daf97084fa1f7b413b070189855a9
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-08-30 16:47:52 -0400

    consoles

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,12 +5,17 @@ const { Post } = require("../../models");
 
 router.post("/create", async (req, res) => {
     try {
+        console.log("Adding post")
+        console.log(req.body);
+
         const postData = await Post.create({
           title: req.body.title,
           body: req.body.body,
           date: Date.now(),
           user_id: req.body.user_id
         });
+
+        console.log(postData)
 
         res.status(200).json(postData);
         
