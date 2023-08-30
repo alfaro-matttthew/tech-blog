514a1f3b2ac7450bd0482d6251c06e137dce71c4
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-08-30 16:53:54 -0400

    fixed event listenere

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,25 +1,23 @@
 const newPostHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#title-newPost').value.trim();
-    const body = document.querySelector('#content-newPost').value.trim();
-    const user_id = document.querySelector('#user_id').value.trim();
-    console.log(user_id);
-  
-    if (title && body) {
-      const response = await fetch('/api/posts/create', {
-        method: 'POST',
-        body: JSON.stringify({ title, body, user_id }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to log in');
-      }
+  event.preventDefault();
+
+  const title = document.querySelector("#title-newPost").value.trim();
+  const body = document.querySelector("#content-newPost").value.trim();
+  const user_id = document.querySelector("#user_id").value.trim();
+  console.log(user_id);
+
+  if (title && body) {
+    const response = await fetch("/api/posts/create", {
+      method: "POST",
+      body: JSON.stringify({ title, body, user_id }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to log in");
     }
-  };
-  document
-    .querySelector('#new-post')
-    .addEventListener('submit', newPostHandler);  
+  }
+};
+document.querySelector("#new-post").addEventListener("submit", newPostHandler);
